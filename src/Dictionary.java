10c69c8f228eb8c03e9aaf3ab8ff98681051b7bb
egbor <egborosebhulimen8@gmail.com>
2023-05-07 23:58:33 -0300

    a) Made the dictionary class a singleton, as more instances of the dictionary are not usefully and inefficient, as the dictionary creation itself takes time.
    b) Added the ability to get the next characters node from the current node, and changed the "searchDictionaryHelper" method to use that instead of just using the map directly for easy flexibility in the future
    
    c) Did a bit of code cleanup, and documentation.

--- src/Dictionary.java
+++ src/Dictionary.java
@@ -1,10 +1,14 @@
+import org.jetbrains.annotations.NotNull;
+
 import java.io.File;
 import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.Optional;
 import java.util.Scanner;
 
 /**
  * @author Egbor Osebhulimen
+ * @date 2023-05-02
  * This dictionary is implemented using the  {<a href="https://en.wikipedia.org/wiki/Trie">trie data structure</a>}
  * Algorithm	Average	Worst case
  * Space	O(n)	O(n)
@@ -14,33 +18,81 @@ import java.util.Scanner;
  */
 public class Dictionary
 {
-    File dictionary;
-    TrieNode root;
+    private final File dictionaryTxt; // File that stores the words if the dictionary in a txt file
+    private final TrieNode root; // Top level of the dictionary
 
+    //  Stores a single instance of the dictionary object
+    private static final Dictionary DICTIONARY_INSTANCE = new Dictionary();
+
+    /**
+     * Represents a node in a trie data structure for storing a characters and its preceding character options.
+     */
     static class TrieNode{
+        // A mapping from characters to the next nodes in the trie
         LinkedHashMap<Character, TrieNode> suffix;
+
+        // Whether this node represents a possible end of a string
         boolean isPossibleEnd;
-        char currChar;
-        public TrieNode()
-        {
+
+        // The character represented by this node
+        char currentChar;
+
+        /**
+         * Constructs a new trie node.
+         */
+        public TrieNode() {
             suffix = new LinkedHashMap<>();
             isPossibleEnd = false;
         }
 
-        public TrieNode add(char c,TrieNode next) {
-            next.currChar = c;
+        /**
+         * Adds a new character to the trie below this node, creating new nodes as needed.
+         * Returns the next node in the trie after adding the character.
+         *
+         * @param c the character to add
+         * @param next the next node to link to this one for the given character
+         * @return the next node in the trie after adding the character
+         */
+        public TrieNode add(char c, @NotNull TrieNode next) {
+            next.currentChar = c;
             suffix.putIfAbsent(c, next);
             return suffix.get(c);
         }
+
+        /**
+         * Returns the next node in the trie for the given character, if one exists.
+         *
+         * @param c the character to look for
+         * @return an {@code Optional} containing the next node in the trie for the given character,
+         *         or {@code Optional.empty()} if no such node exists
+         */
+        public TrieNode getNextCharNode(char c){return suffix.get(c);}
+
     }
 
-    public Dictionary()
+
+
+    /**
+     * Private constructor to prevent direct instantiation of the class from outside.
+     * Initializes the dictionary by reading the words from a text file and adding them to the trie tree.
+     */
+    private Dictionary()
     {
-        String dictionaryPath = "src/english3.txt";
-//        String dictionaryPath = "C:\\Users\\egbor\\Videos\\WordPuzzleSolver\\src\\english3.txt";
-        dictionary = new File(dictionaryPath);
+        String currentDirectory = System.getProperty("user.dir"); // Uses this to get the base dir eg "~/.../WordPuzzleSolver"
+        String dictionaryPath = currentDirectory+"\\src\\english3.txt";
+        dictionaryTxt = new File(dictionaryPath);
         root = new TrieNode();
         fillDictionary();
+    }
+
+
+    /**
+     * Returns the single instance of the dictionary.
+     * This method is thread-safe and ensures only one instance is created across all threads.
+     * @return The single instance of the dictionary.
+     */
+    public static Dictionary getDictionaryInstance() {
+        return DICTIONARY_INSTANCE;
     }
 
     /**
@@ -48,8 +100,7 @@ public class Dictionary
      */
     private void fillDictionary()
     {
-        int i = 0;
-        try(Scanner input = new Scanner(dictionary);){
+        try(Scanner input = new Scanner(dictionaryTxt)){
             while(input.hasNextLine()){
                 String currentWord = input.nextLine();
                 if (currentWord.length() >= 1) {
@@ -68,10 +119,8 @@ public class Dictionary
      * @param index The indexOf the word to be added
      * @param currentLevel  Level of the tree to insert into
      */
-    private void addToDictionary(String currentWord, int index, TrieNode currentLevel)
+    private void addToDictionary(@NotNull String currentWord, int index, @NotNull TrieNode currentLevel)
     {
-        int currentIndex = currentWord.length() - index;
-//        System.out.print(currentWord.charAt(currentIndex));
         // adds to the current level
         char currentChar = Character.toLowerCase(currentWord.charAt(index));
         TrieNode insertedVal = currentLevel.add(currentChar, new TrieNode());
@@ -93,24 +142,43 @@ public class Dictionary
         printDictionaryHelper(root.suffix,new StringBuilder());
     }
 
+    public boolean searchDictionary(@NotNull String word){
+        return word.length() > 0 && searchDictionaryHelper(word.toLowerCase(), 0, root);
+    }
+
+
+    // -------------------------- Helpers --------------------------------------//
+    private boolean searchDictionaryHelper(@NotNull String word, int currentSearchIndex, @NotNull TrieNode currLevel) {
+        char currChar = word.charAt(currentSearchIndex);
+
+        // Word not found
+        TrieNode currCharNode = currLevel.getNextCharNode(currChar);
+        if (currCharNode == null) return false;
+        else {  // Word end found
+            if (currentSearchIndex==word.length()-1) {
+                return currCharNode.isPossibleEnd;
+            }
+            return searchDictionaryHelper(word,++currentSearchIndex,currCharNode);
+        }
+    }
+
     /**
      * Helper function that prints the dictionary
      *
      * @param currLevelList Map containing options for each level in the tree
      * @param wordToPrint Sequence of word that is being printed
      */
-    private void printDictionaryHelper(LinkedHashMap<Character, TrieNode> currLevelList, StringBuilder wordToPrint)
+    private void printDictionaryHelper(@NotNull LinkedHashMap<Character, TrieNode> currLevelList, StringBuilder wordToPrint)
     {
         for (Map.Entry<Character, TrieNode> currLevelOptions : currLevelList.entrySet()){
             TrieNode currPosition = currLevelOptions.getValue();
-            wordToPrint.append(currPosition.currChar);
+            wordToPrint.append(currPosition.currentChar);
 
             if (currPosition.isPossibleEnd){
                 System.out.println(wordToPrint);
             }
 
             printDictionaryHelper(currPosition.suffix, wordToPrint);
-//            printDictionary();
         }
         // If no more words can be formed on that level of the tree
         if (wordToPrint.length()>0){
